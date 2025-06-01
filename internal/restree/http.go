2e2fe25cd4ea8bf3c28f6a1f5838ab0adf293d7a
Kamil Kozioł <koziol.kamil@protonmail.com>
2025-06-01 19:02:08 +0200

    feat(scripts): add _before.sh script support

--- internal/restree/http.go
+++ internal/restree/http.go
@@ -1,11 +1,13 @@
 package restree
 
 import (
+	"bufio"
 	"bytes"
 	"fmt"
 	"io"
 	"maps"
 	"os"
+	"os/exec"
 	"path/filepath"
 	"regexp"
 	"strings"
@@ -15,6 +17,7 @@ import (
 )
 
 const HeadersFileName = "_headers.http"
+const BeforeScriptFileName = "_before.sh"
 
 func LoadHTTPRequest(path string) (*httpparser.HTTPRequest, error) {
 	template, err := os.ReadFile(path)
@@ -73,6 +76,97 @@ func HandleHTTPHeaders(data io.Reader) (httpparser.HTTPHeaders, error) {
 	return parsed, nil
 }
 
+func runScript(scriptPath string) (string, string, error) {
+	cmd := exec.Command("bash", scriptPath) // Use "sh" if preferred
+
+	var stdoutBuf, stderrBuf bytes.Buffer
+	cmd.Stdout = &stdoutBuf
+	cmd.Stderr = &stderrBuf
+
+	err := cmd.Run()
+	if err != nil {
+		return "", "", err
+	}
+
+	return stdoutBuf.String(), stderrBuf.String(), err
+}
+
+// parseScriptEnvOutput parses the output as env
+func parseScriptEnvOutput(out string) (map[string]string, error) {
+	envMap := make(map[string]string)
+	scanner := bufio.NewScanner(bytes.NewBufferString(out))
+	for scanner.Scan() {
+		line := scanner.Text()
+		if strings.Contains(line, "=") {
+			parts := strings.SplitN(line, "=", 2)
+			if len(parts) == 2 {
+				key := parts[0]
+				value := parts[1]
+				envMap[key] = value
+			}
+		}
+	}
+
+	return envMap, nil
+}
+
+func processDirectory(currentPath string) (httpparser.HTTPHeaders, error) {
+	entries, err := os.ReadDir(currentPath)
+	if err != nil {
+		return nil, fmt.Errorf("failed to read dir: %s", currentPath)
+	}
+
+	headers := httpparser.HTTPHeaders{}
+
+	var headersPath, beforePath string
+
+	// find the files in directory
+	for _, entry := range entries {
+		if entry.IsDir() {
+			continue
+		}
+
+		name := entry.Name()
+		entryPath := filepath.Join(currentPath, name)
+
+		switch entry.Name() {
+		case HeadersFileName:
+			headersPath = entryPath
+		case BeforeScriptFileName:
+			beforePath = entryPath
+		}
+
+	}
+
+	// run the before script first
+	// TODO: pass the current headers to the scripts as a variable
+	if beforePath != "" {
+		stdout, stderr, err := runScript(beforePath)
+		if err != nil {
+			return nil, fmt.Errorf("failed to execute before script: %s\n%s", err, stderr)
+		}
+		exportedEnvs, err := parseScriptEnvOutput(stdout)
+		if err != nil {
+			return nil, fmt.Errorf("failed to parse envs: %s", err)
+		}
+
+		// set the variables
+		for key, value := range exportedEnvs {
+			os.Setenv(key, value)
+		}
+	}
+
+	// run parse the headers
+	if headersPath != "" {
+		headers, err = LoadHTTPHeaders(headersPath)
+		if err != nil {
+			return nil, fmt.Errorf("Failed to load template %s: %s", headersPath, err)
+		}
+	}
+
+	return headers, nil
+}
+
 func RecursiveRead(from string, to string) (*httpparser.HTTPRequest, error) {
 	traversalStr, found := strings.CutPrefix(to, from)
 	if !found {
@@ -88,24 +182,12 @@ func RecursiveRead(from string, to string) (*httpparser.HTTPRequest, error) {
 	currentPath := from
 	for _, dir := range dirs {
 		currentPath = filepath.Join(currentPath, dir)
-
-		entries, err := os.ReadDir(currentPath)
+		directoryHeaders, err := processDirectory(currentPath)
 		if err != nil {
-			return nil, fmt.Errorf("Failed to read dir: %s\n", currentPath)
+			return nil, fmt.Errorf("unable to process dir: %s", err)
 		}
+		maps.Copy(headers, directoryHeaders)
 
-		for _, entry := range entries {
-			name := entry.Name()
-			if name == HeadersFileName {
-				headersPath := filepath.Join(currentPath, name)
-				currentHeaders, err := LoadHTTPHeaders(headersPath)
-				if err != nil {
-					return nil, fmt.Errorf("Failed to load template %s: %s\n", headersPath, err)
-				}
-
-				maps.Copy(headers, currentHeaders)
-			}
-		}
 	}
 
 	httpFile, err := LoadHTTPRequest(to)
